424909f67d4297fe7e2ef36acb78f784937a4991
sasie59 <sasie59@gmail.com>
2018-03-27 00:17:39 +0800

    簡易留言板 架設

---
+++ work2.js
@@ -0,0 +1,20 @@
+function sendMessage(e) {
+  // prevent 防止 default 預設
+  e.preventDefault();
+  const messageName = document.getElementById("name");
+  const messageText = document.getElementById("text");
+
+  const mesesageBlock = document.getElementById('mesesage-block');
+  const newMessage =
+    '<div class="message">' +
+      '<div class="name">' + messageName.value + '</div>' +
+      '<div class="text">' + messageText.value.replace("\n", "<br />") + '</div>' +
+    '</div>';
+
+  // e = Event 觸發事件變數 (包括了觸發者…等屬性)
+  mesesageBlock.innerHTML = newMessage + mesesageBlock.innerHTML;
+
+  messageName.value = "";
+  messageText.value = "";
+}
+document.getElementById("message-form").addEventListener("submit", sendMessage);
