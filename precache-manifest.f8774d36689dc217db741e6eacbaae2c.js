aa78c04dfe8092032339b76401570e332956bf57
sasie59 <sasie59@gmail.com>
2020-03-23 22:40:05 +0800

    push homework

---
+++ precache-manifest.f8774d36689dc217db741e6eacbaae2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba01e2d0cc43bbbc78e7a11048813862",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2fdb95e6e6f959ea979",
+    "url": "/static/css/main.e4d7337c.chunk.css"
+  },
+  {
+    "revision": "61200a818f5613c4c444",
+    "url": "/static/js/2.f6f3e1c1.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.f6f3e1c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2fdb95e6e6f959ea979",
+    "url": "/static/js/main.74e49a1a.chunk.js"
+  },
+  {
+    "revision": "77bcc22f8893c9769cbf",
+    "url": "/static/js/runtime-main.241e5dc1.js"
+  }
+]);
